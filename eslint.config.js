726cf078670e0b798382c1355880d57631dda935
Dmitry Maslennikov <maslennikovdm@gmail.com>
2024-02-09 00:55:11 +0300

    Initial Vite + Preact + TS + Eslint + Tailwind

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import stylistic from '@stylistic/eslint-plugin'
+import parser from '@typescript-eslint/parser'
+
+export default [
+    {
+        files: [
+            'src/**/*.@(ts|tsx|js|jsx)',
+            'vite.config.ts'
+        ],
+        languageOptions: {
+            parser
+        }
+    },
+    stylistic.configs.customize({
+        indent: 4,
+        quotes: 'single',
+        semi: false,
+        jsx: true,
+        commaDangle: 'never'
+    })
+]
---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+import { render } from 'preact'
+import './main.css'
+
+render(<div>cool</div>, document.getElementById('app')!)
