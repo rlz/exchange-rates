56cf46fb042624080c85b68f264497d4233c719f
Dmitry Maslennikov <maslennikovdm@gmail.com>
2025-02-09 16:24:51 +0300

    Fix tailwind build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [preact()],
+    plugins: [
+        preact(),
+        tailwindcss()
+    ],
     base: '/exchange-rates/'
 })
